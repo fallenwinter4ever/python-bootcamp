25e8f1879e87c8275de3d4e36e055985866f34ea
Sarang Vaisakh <vaisakhsarang@gmail.com>
2026-08-04 15:11:22 +0530

    Update to _do_manager.py

--- Projects/to _do_manager.py
+++ Projects/to _do_manager.py
@@ -3,10 +3,11 @@ while True:
  print("==== To_do_Manager ====")
  print("1: Add a task")
  print("2: View tasks")
- print("3: Exit")
+ print("3: Delete a task")
+ print("4: Exit")
  choice = int(input("Choose an option: "))
  if choice == 1:
-    print("you chose option 1")
+    print("\n=== Add Task ===")
     task = input("enter the task:")
     save = input("Do you want to save the task? (y/n): ")
     if save.lower() == "y":
@@ -17,7 +18,7 @@ while True:
         print("Task not saved.")
             
  elif choice == 2:
-    print("you chose option 2")
+    print("\n=== View Tasks ===")
     if not tasks:
      print("No tasks found.")
     else:
@@ -26,11 +27,28 @@ while True:
         print(f"{i}. {task}")
    
  elif choice == 3:
-    print("you chose option 3")
+    print("\n=== Delete Task ===")
+    if not tasks:
+       print("No tasks found.")
+    else:
+       print ("Tasks:")
+       for i, task in enumerate(tasks, start=1):
+          print(f"{i}. {task}")
+       task_number = int(input("enter the task number to be deleted: "))
+       if 1 <= task_number <= len(tasks):
+        deleted_task = tasks.pop(task_number - 1)
+        print(f"Task successfully deleted: {deleted_task}")
+       else:
+        print("Invalid task number.")
+
+ elif choice == 4:
+    print("\n=== Exit ===")
     print("Exiting the program...")
     print ("successfully exited")
     break
  else:
     print("Invalid choice")
+
+    
 
  
