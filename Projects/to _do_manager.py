4c858a81b74e9dae1f9231dfa1835d5c0bc5be70
Sarang Vaisakh <vaisakhsarang@gmail.com>
2026-08-05 00:50:38 +0530

    Complete To-Do Manager v1.1

--- Projects/to _do_manager.py
+++ Projects/to _do_manager.py
@@ -1,5 +1,20 @@
 tasks = []
 starred_tasks = []
+def load_tasks():
+    try:
+        with open("tasks.txt", "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                tasks.append(line.strip())
+
+    except FileNotFoundError:
+        pass
+def save_tasks():
+    with open("tasks.txt", "w") as file:
+        for task in tasks:
+            file.write(task + "\n")
+    
+
 def show_menu():
     print("==== To_do_Manager ====")
     print("1: Add a task")
@@ -13,6 +28,7 @@ def add_task():
     save = input("Do you want to save the task? (y/n): ")
     if save.lower() == "y":
          tasks.append(task)
+         save_tasks()
          print("Task saved successfully!")
    
     else:
@@ -36,15 +52,22 @@ def delete_task():
            print ("Tasks:")
            for i, task in enumerate(tasks, start=1):
             print(f"{i}. {task}")
-           task_number = int(input("enter the task number to be deleted: "))
+           try:
+             task_number = int(input("enter the task number to be deleted: "))
+           except ValueError:
+             print("Invalid number. Enter a valid number.")
+             return
+
            if 1 <= task_number <= len(tasks):
             deleted_task = tasks[task_number - 1]
-            tasks.remove(deleted_task)
+            tasks.pop(task_number - 1)
+            save_tasks()
             print(f"Task successfully deleted: {deleted_task}")
-           if deleted_task in starred_tasks:
-            starred_tasks.remove(deleted_task)
+            if deleted_task in starred_tasks:
+             starred_tasks.remove(deleted_task)
            else:
             print("Invalid task number.")
+            return
 def star_task():
    print("\n=== Star Task ===")
    if not tasks:
@@ -53,20 +76,29 @@ def star_task():
       print("Tasks:")
       for i, task in enumerate(tasks, start=1):
        print(f"{i}. {task}")
+      try:
        task_number = int(input("Enter the task number to star: "))
+      except ValueError:
+       print("Invalid number. Enter a valid number.")
+       return
       if 1 <= task_number <= len(tasks):
-       starred_task = tasks[task_number - 1]
-       starred_tasks.append(starred_task)
-       print(f"Task successfully starred: {starred_task}")
+        starred_task = tasks[task_number - 1]
+        starred_tasks.append(starred_task)
+        print(f"Task successfully starred: {starred_task}")
       else:
        print("Invalid task number.")
 def exit_program():
     print("\n=== Exit ===")
     print("Exiting the program...")
     print ("successfully exited")
+load_tasks()
 while True:
  show_menu()
- choice = int(input("Choose an option: "))
+ try:
+    choice = int(input("Choose an option: "))
+ except ValueError:
+      print("Invalid input. Please enter a number.")
+      continue
  if choice == 1:
      add_task()
  elif choice == 2:
