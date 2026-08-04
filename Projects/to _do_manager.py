cc776ccf26291cddb661f44f74ac7ddbbb562f1e
Sarang Vaisakh <vaisakhsarang@gmail.com>
2026-08-04 15:49:45 +0530

    Implement task starring and improve UI

--- Projects/to _do_manager.py
+++ Projects/to _do_manager.py
@@ -1,10 +1,12 @@
 tasks = []
+starred_tasks = []
 while True:
  print("==== To_do_Manager ====")
  print("1: Add a task")
  print("2: View tasks")
  print("3: Delete a task")
- print("4: Exit")
+ print("4: Star a task")
+ print("5: Exit")
  choice = int(input("Choose an option: "))
  if choice == 1:
     print("\n=== Add Task ===")
@@ -24,7 +26,10 @@ while True:
     else:
      print("Tasks:")
      for i, task in enumerate(tasks, start=1):
-        print(f"{i}. {task}")
+        if task in starred_tasks:
+            print(f"{i} * {task}")
+        else:
+            print(f"{i}. {task}")
    
  elif choice == 3:
     print("\n=== Delete Task ===")
@@ -36,16 +41,35 @@ while True:
           print(f"{i}. {task}")
        task_number = int(input("enter the task number to be deleted: "))
        if 1 <= task_number <= len(tasks):
-        deleted_task = tasks.pop(task_number - 1)
+        deleted_task = tasks[task_number - 1]
+        tasks.remove(deleted_task)
         print(f"Task successfully deleted: {deleted_task}")
+        if deleted_task in starred_tasks:
+            starred_tasks.remove(deleted_task)
        else:
         print("Invalid task number.")
 
  elif choice == 4:
-    print("\n=== Exit ===")
-    print("Exiting the program...")
-    print ("successfully exited")
-    break
+      print("\n=== Star Task ===")
+      if not tasks:
+         print("No tasks found.")
+      else:
+         print("Tasks:")
+         for i, task in enumerate(tasks, start=1):
+            print(f"{i}. {task}")
+         task_number = int(input("Enter the task number to star: "))
+         if 1 <= task_number <= len(tasks):
+            starred_task = tasks[task_number - 1]
+            starred_tasks.append(starred_task)
+            print(f"Task successfully starred: {starred_task}")
+         else:
+            print("Invalid task number.")
+
+ elif choice == 5:
+           print("\n=== Exit ===")
+           print("Exiting the program...")
+           print ("successfully exited")
+           break
  else:
     print("Invalid choice")
 
